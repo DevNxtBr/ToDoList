00101c140b3ddb08050d5a4d70397c495d5e70e0
wesleybruno7 <wesleybruno7@terra.com.br>
2020-07-31 23:09:21 -0300

    inserindo dados de conexao heroku

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,35 @@
 module.exports = {
 
-    development: {
-      client: 'postgresql',
-      connection: {
-        host: 'localhost',
-        database: 'todolist',
-        user:     'postgres',
-        password: 'master'
-      },
-      migrations: {
-        directory: './src/database/migrations'
-      },
-      useNullAsDefault: true,
-    },
-  
     // development: {
     //   client: 'postgresql',
     //   connection: {
-    //     host: '',
-    //     database: '',
-    //     user:     '',
-    //     password: '',
-    //     ssl: {
-    //       rejectUnauthorized: false
-    //     },
+    //     host: 'localhost',
+    //     database: 'todolist',
+    //     user:     'postgres',
+    //     password: 'master'
     //   },
     //   migrations: {
     //     directory: './src/database/migrations'
     //   },
     //   useNullAsDefault: true,
     // },
+  
+    development: {
+      client: 'postgresql',
+      connection: {
+        host: 'ec2-34-225-162-157.compute-1.amazonaws.com',
+        database: 'd60l0sr010u0pp',
+        user:     'wnhywxytoncohk',
+        password: '8bca8414dd25e97050b49205d285251d673103990f8d0fec960f5a96ecc2173b',
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+      migrations: {
+        directory: './src/database/migrations'
+      },
+      useNullAsDefault: true,
+    },
     
   };
   
